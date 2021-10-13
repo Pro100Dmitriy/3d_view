f197355ad258a0eb82e1181ec2ffe0df71a4f59b
Dmitry Shestak <dimonshestak160201@gmail.com>
2021-10-13 07:36:22 +0300

    save

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,10 @@ import '../sass/style.sass'
 
 import * as THREE from 'three'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 50000)
 camera.position.z = 100
 
 const renderer = new THREE.WebGL1Renderer({alpha: true, antialias: true})
@@ -14,11 +15,11 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.domElement.setAttribute("id", 'object')
 document.body.appendChild( renderer.domElement )
 
-const aLight = new THREE.AmbientLight(0x404040, 100)
+const aLight = new THREE.AmbientLight(0x404040, 1)
 scene.add(aLight)
 
-const pLight = new THREE.PointLight(0xFFFFFF, 100)
-pLight.position.set(0, -3, 7)
+const pLight = new THREE.PointLight(0xFFFFFF, 1)
+pLight.position.set(0, 100, 80)
 scene.add(pLight)
 
 const helper = new THREE.PointLightHelper(aLight)
@@ -26,16 +27,16 @@ scene.add(helper)
 
 let loader = new OBJLoader()
 let obj = null
-
 loader.load( 
-	'models/01_Design.obj',
-	function ( object ) {
+	'test.obj',
+	object => {
         obj = object
-        obj.scale.set(1, 1, 1 )
+        obj.scale.set(10, 10, 10 )
         obj.position.x = 0
-        obj.position.y = 0
+        obj.position.y = -20
         obj.position.z = 50
-		scene.add( obj );
+		scene.add( obj )
+        renderer.render(scene, camera)
 	}
 );
 
@@ -43,9 +44,7 @@ const geometry = new THREE.TorusGeometry( 10,3,16,100 )
 const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } )
 const torus = new THREE.Mesh( geometry, material )
 torus.position.z = 50
-
-
-scene.add(torus)
+//scene.add(torus)
 
 renderer.render(scene, camera)
 
@@ -53,12 +52,11 @@ renderer.render(scene, camera)
 function animation(){
     requestAnimationFrame( animation )
 
-    torus.rotation.x += 0.01
-    torus.rotation.y += 0.005
-    torus.rotation.z += 0.01
+    obj.rotation.x += 0
+    obj.rotation.y += 0.005
+    obj.rotation.z += 0
 
     renderer.render(scene, camera)
-
 }
 
 animation()
