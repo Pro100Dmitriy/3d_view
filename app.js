d92abe2082809af19e0d8d845819696c28ccef97
Dmitry Shestak <dimonshestak160201@gmail.com>
2022-01-29 09:49:02 +0300

    save

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
 const positionRoutes = require('./routes/position')
@@ -16,5 +17,13 @@ app.use(bodyParser.json())
 
 app.use('/api/position', positionRoutes)
 app.use('/api/scene', sceneRoutes)
+
+if( process.env.NODE_ENV === 'production' ){
+    app.use(express.static('.client/dist'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
 
 module.exports = app
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://shop-admin:3dshop@cluster0-shard-00-00.efj9r.mongodb.net:27017,cluster0-shard-00-01.efj9r.mongodb.net:27017,cluster0-shard-00-02.efj9r.mongodb.net:27017/3dshopdb?ssl=true&replicaSet=atlas-g2r2fg-shard-0&authSource=admin&retryWrites=true&w=majority'
+if( process.env.NODE_ENV === 'production' ){
+    module.exports = require('./keys.prod')
+}else{
+    module.exports = require('./keys.dev')
 }
